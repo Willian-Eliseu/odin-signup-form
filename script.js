b4ef6a8fecf94d1da6efa6387a73575c8c285005
Willian Eliseu da Silva <w.eliseu@gmail.com>
2026-01-26 20:18:11 -0300

    added the css file to format the project

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const teste = "arquivo de script";
+const password = document.querySelector("#password");
+const passwordConfirm = document.querySelector("#passwordConfirm");
+const formulary = document.querySelector("#form-subscribe");
+
+passwordConfirm.addEventListener("keyup", () => {
+  if (password.value === passwordConfirm.value) {
+    passwordConfirm.classList.remove("password-check");
+    passwordConfirm.classList.add("password");
+  } else {
+    passwordConfirm.classList.remove("password");
+    passwordConfirm.classList.add("password-check");
+  }
+});
+
+formulary.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    if (password.value !== passwordConfirm.value) {
+        alert('Passwords must be the same');
+    }
+
+    alert('Formulary submited successfully');
+    formulary.reset();
+
+});
